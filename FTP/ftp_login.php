b5a6f76cd1fee2c4d9a5905a07fba2766a4fcfc6
ramiro <navarretee1133@gmail.com>
2023-07-20 16:34:54 -0300

    introduction

---
+++ FTP/ftp_login.php
@@ -0,0 +1,21 @@
+<?php
+
+// Creamos variables con los datos del servidor FTP y nuestros datos de acceso
+$ftpHost = 'ftp.midominioweb.com';
+$ftpUsername = 'usuarioftp';
+$ftpPassword = '*****';
+
+// Establecemos la conexion don el servidor
+$connId = ftp_connect($ftpHost) or die("No se pudo conectar a $ftpHost");
+
+// Si la conexion se establece, tratamos de hacer login con nuestros datos
+if(@ftp_login($connId, $ftpUsername, $ftpPassword)){
+    echo "Conectado como $ftpUsername@$ftpHost";
+} else {
+    echo "No se a podido establecer la conexion con @ftpUsername";
+}
+
+// Una vez terminado, cerrramos la conexión
+ftp_close($connId);
+
+?>
